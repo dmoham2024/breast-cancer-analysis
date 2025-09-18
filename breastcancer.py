a10a9c22a7a86e2b1d43976d477d1345cf199781
dmoham2024 <danish@danishs-mbp.lan>
2025-09-18 00:40:30 -0500

    pandas saved

--- breastcancer.py
+++ breastcancer.py
@@ -1,3 +1,17 @@
 print('Breast cancer')
 # testing everything out#
 
+#import libraries and dependecies 
+import pandas as pd 
+from pathlib import Path 
+
+
+#read the csv files into Data frames
+breastcancer_path = Path('/Users/danish/Desktop/breast-cancer-analysis/breast+cancer+wisconsin+diagnostic/wdbc.data')
+breastcancer = pd.read_csv(breastcancer_path, header=None)
+
+
+#Display data
+breastcancer.head()
+
+
